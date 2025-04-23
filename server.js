cc432f4d566338b3e7b107e5e681cb83865e7493
Aman1685 <dhimanaman1685@gmail.com>
2025-04-23 19:47:16 +0530

    adding paths and renaming

--- server.js
+++ server.js
@@ -17,6 +17,55 @@ app.get('/undergraduate', (req, res) => {
 app.get('/curriculum', (req, res) => {
     res.sendFile(path.join(__dirname, 'curriculum.html'));
 });
-app.listen(PORT, () => {
+
+app.get('/Campus_life', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Campus_life.html'));
+});
+
+app.get('/admissions', (req, res) => {
+    res.sendFile(path.join(__dirname, 'admissions.html'));
+});
+
+app.get('/Photo_Gallery', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Photo_Gallery.html'));
+});
+
+app.get('/Contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Contact.html'));
+});
+app.get('/mission', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mission.html'));
+});
+
+app.get('/administration', (req, res) => {
+    res.sendFile(path.join(__dirname, 'administration.html'));
+});
+
+app.get('/admission', (req, res) => {
+    res.sendFile(path.join(__dirname, 'admission.html'));
+});
+
+app.get('/Faculty_Profiles', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Faculty_Profiles.html'));
+});
+
+app.get('/videos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'videos.html'));
+});
+
+app.get('/event', (req, res) => {
+    res.sendFile(path.join(__dirname, 'event.html'));
+});
+
+app.get('/tender', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tender.html'));
+});
+
+app.get('/news', (req, res) => {
+    res.sendFile(path.join(__dirname, 'news.html'));
+});
+
+
+    app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
